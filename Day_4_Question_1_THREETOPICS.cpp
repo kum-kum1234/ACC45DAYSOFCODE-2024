baa15038980f7420395514e4fc0525288ff581d3
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-01 19:15:20 +0530

    Add files via upload

---
+++ Day_4_Question_1_THREETOPICS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+       int A,B,C,X;
+       cin>>A>>B>>C>>X;
+       if(A==X || B==X || C==X){
+           cout<<"Yes"<<endl;
+       }
+       else{
+           cout<<"No"<<endl;
+       }
+       return 0;
+
+}
