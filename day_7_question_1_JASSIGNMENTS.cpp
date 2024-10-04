2d332f8a3e4ae94e0d032f285078ed1bacf9504d
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-04 19:33:38 +0530

    Add files via upload

---
+++ day_7_question_1_JASSIGNMENTS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(10-x>=3){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	    
+}
+	    return 0;
+}
