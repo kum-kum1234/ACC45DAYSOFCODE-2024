18488f779afd3a80d11fe968c0c6173729d5d9f2
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-06 22:25:22 +0530

    Add files via upload

---
+++ day_9_question_2_ADD12GAME.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+   int T;
+   cin>>T;
+   
+   while(T--){
+       
+       int N;
+       cin>>N;
+       
+       if(N==1) cout<< "ALICE"<<"\n";
+       else if(N==2) cout<<"BOB"<<"\n";
+       else cout<< "BOB"<<"\n";
+   }
+   return 0;
+}
