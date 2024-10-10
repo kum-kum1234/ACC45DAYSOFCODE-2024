7f3b8ddf00fd0dc2fd4df414f90004a137f28826
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-10 19:30:04 +0530

    Add files via upload

---
+++ day_13_question_1_EXPERT.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(2*Y>=X){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
