a32593aa34e8b07f7a2eb7f8fef57b43465f74a0
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-21 20:05:14 +0530

    Add files via upload

---
+++ day_19_question_1_CHEFSCORE.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int N,X,Y;
+	    cin>>N>>X>>Y;
+	    if(Y%X==0){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+
+}
