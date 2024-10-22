f18e95f2391e977cc74115882a6bf6e20fbccb45
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-10-22 19:19:17 +0530

    Add files via upload

---
+++ day_20_question_1_CHEFGAMES.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int R1,R2,R3,R4;
+	    cin>>R1>>R2>>R3>>R4;
+	    if(R1==0 && R2==0 && R3==0 && R4==0){
+	        cout<<"In"<<endl;
+	    }
+	    else{
+	        cout<<"out"<<endl;
+	    }
+	}
+	return 0;
+
+}
