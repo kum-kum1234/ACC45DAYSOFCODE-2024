f1db3a5c2801a881bf1dbcd6cf1d59181540f159
kum-kum1234 <145197845+kum-kum1234@users.noreply.github.com>
2024-11-04 21:18:17 +0530

    Add files via upload

---
+++ day_33_question_1_NIBBLE.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     while(t--){
+         int N;
+         cin>>N;
+         if(N%4==0){
+             cout<<"Good"<<endl;
+         }
+         else{
+             cout<<"Not good"<<endl;
+         }
+     }
+     return 0;
+}
